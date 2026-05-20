1c1e935e35bdd1fb732f0749ce2986320a519b6a
msmraqeeb <msmraqeeb@gmail.com>
2026-05-20 14:53:53 +0600

    1.0.2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://upbkfhtbbqxsloihejvz.supabase.co';
-const supabaseAnonKey = 'sb_publishable_FVSNVq1PgVxtkrerDHO_Qw_5I_Qo83X';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://upbkfhtbbqxsloihejvz.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_FVSNVq1PgVxtkrerDHO_Qw_5I_Qo83X';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
