b4a585cde20b2b0fb382a5d5a85a2e3c1055b483
msmraqeeb <msmraqeeb@gmail.com>
2026-05-23 13:03:14 +0600

    1.0.12

--- pages/Products.tsx
+++ pages/Products.tsx
@@ -516,7 +516,7 @@ const Products: React.FC = () => {
                 </button>
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
                 {filteredProducts.map(product => (
                   <ProductCard key={product.id} product={product} />
                 ))}
