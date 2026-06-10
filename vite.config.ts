aac26f8b6e272aba405914f151cd8f2b57018b30
msmraqeeb <msmraqeeb@gmail.com>
2026-06-10 11:20:21 +0600

    PWA updated 1.2

--- vite.config.ts
+++ vite.config.ts
@@ -22,18 +22,18 @@ export default defineConfig({
     categories: ['shopping', 'lifestyle'],
     id: 'https://zerobaby.com.bd/',
     icons: [
-      {
-        src: 'https://ik.imagekit.io/vrtbi4wsn/store/zerobaby-fav.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'https://ik.imagekit.io/vrtbi4wsn/store/zerobaby-fav.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable'
-      }
-    ],
+  {
+    src: 'https://ik.imagekit.io/vrtbi4wsn/store/zerobaby-icon-192.png',
+    sizes: '192x192',
+    type: 'image/png'
+  },
+  {
+    src: 'https://ik.imagekit.io/vrtbi4wsn/store/zerobaby-icon-512.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'any maskable'
+  }
+],
     screenshots: [
       {
         src: 'https://ik.imagekit.io/vrtbi4wsn/store/screenshot-wide.png',
