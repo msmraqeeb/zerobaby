31ac879fbe3b162b6d8580c954341ce7ef079643
msmraqeeb <msmraqeeb@gmail.com>
2026-06-18 13:00:34 +0600

    2.8.4

--- App.tsx
+++ App.tsx
@@ -58,13 +58,7 @@ const AppContent: React.FC = () => {
     }
   }, [pathname]);
 
-  if (loading) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-rose-500"></div>
-      </div>
-    );
-  }
+
 
   return (
     <div className="min-h-screen flex flex-col font-sans">
